25a0d8eaef3296eec415818ccc87c2e787909786
Abdul Rehman <pakkhan9009@gmail.com>
2024-05-24 22:59:15 +0500

    Age to Days Converter JS

--- week-3/Task-10/script.js
+++ week-3/Task-10/script.js
@@ -1,24 +1,26 @@
-const calculateAge = () => {
-    const date = parseInt(document.getElementById('date-field').value);
-    const month = document.getElementById('month-field').value;
-    const year = parseInt(document.getElementById('year-field').value);
+"use strict";
 
-    if (!date || !month || !year || date < 1 || date > 31 || year < 1900 || year > 2025) {
-        console.log('The Provided Date of Birth is Invalid.');
+const calculateAge = () => {
+    const date = document.getElementById('date-field').value;
+    const month = document.getElementById('month-field').value;
+    const year = document.getElementById('year-field').value;
+
+    if (date < 1 || date > 31 || year < 1900 || year > 2025) {
+        alert('The Provided Date of Birth is Invalid.');
         return;
     }
 
     const dateOfBirth = new Date(`${month} ${date} ${year}`);
     const today = new Date();
-
+    
     // Entered date of birth is incorrect
     // if (dateOfBirth > today) {
-    //     alert('The date of birth cannot be in the future');
-    //     return;
-    // }
-
-    const difference = today - dateOfBirth.getTime();
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-
-    document.getElementById('result').textContent = `YOU ARE ${days} DAYS OLD...`;
-};
+        //     alert('The date of birth cannot be in the future');
+        //     return;
+        // }
+        
+        const difference = today - dateOfBirth.getTime();
+        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+        
+        document.getElementById('result').textContent = `YOU ARE ${days} DAYS OLD...`;
+    };
